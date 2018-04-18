0de80e630c9aac173afac995b4fba6805b98b73a
jcorpac <jcorpac@gmail.com>
2018-04-17 22:05:55 -0500

    Implemented recommendations for Page-reader Accessibility

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -98,7 +98,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
     const container = document.getElementById('reviews-container');
-    const title = document.createElement('h2');
+    const title = document.createElement('h3');
     title.innerHTML = 'Reviews';
     container.appendChild(title);
 
@@ -145,7 +145,7 @@ createReviewHTML = (review) => {
 fillBreadcrumb = (restaurant = self.restaurant) => {
     const breadcrumb = document.getElementById('breadcrumb');
     const li = document.createElement('li');
-    li.innerHTML = restaurant.name;
+    li.innerHTML = `<a href="#" aria-current="page">${restaurant.name}</a>`;
     breadcrumb.appendChild(li);
 }
 
